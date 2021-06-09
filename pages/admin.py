c164db84946481c12ca7c1c1f0c1740866a795a3
reddyrekha <reddyrekha1991@gmail.com>
2021-06-09 07:55:40 -0400

    models added

--- pages/admin.py
+++ pages/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
-
+from .models import team
+from django.utils.html import format_html
 # Register your models here.
+class teamAdmin(admin.ModelAdmin):
+    def thumbnail(self,object):
+        return format_html('<img src="{}" width="50" style="border-radius:50px;" />'.format(object.image.url))
+    
+    thumbnail.short_desricription = 'image'
+    list_display=('id','thumbnail','first_name','designation','created_date')
+    list_display_links = ('first_name','id','thumbnail')
+    search_fields =('first_name','last_name','designation')
+    list_filter = ('designation',)
+admin.site.register(team,teamAdmin)
--- pages/views.py
+++ pages/views.py
@@ -1,15 +1,21 @@
 from django.shortcuts import render
+from .models import team
 
 # Create your views here.
 
 def home(request):
-    return render(request,'pages/home.html')
+    teams = team.objects.all()
+    teamlist= {'teams': teams}
+    return render(request,'pages/home.html',teamlist)
 
 def about(request):
-    return render(request,'pages/about.html')
+    teams = team.objects.all()
+    teamlist = {'teams': teams}
+    return render(request,'pages/about.html',teamlist)
 
 def services(request):
     return render(request,'pages/services.html')
 
 def contact(request):
     return render(request,'pages/contact.html')
+
